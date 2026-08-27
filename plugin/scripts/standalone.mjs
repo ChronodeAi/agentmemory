c8263ed850d0fa96df9b3c9f7192555624214f35
ChronodeAi <ChronodeAi@users.noreply.github.com>
2026-08-27 09:28:04 -0500

    chore(release): 0.9.30-chronode.7

--- plugin/scripts/standalone.mjs
+++ plugin/scripts/standalone.mjs
@@ -1761,7 +1761,7 @@ function getAllTools() {
 }
 //#endregion
 //#region src/version.ts
-const VERSION = "0.9.30-chronode.6";
+const VERSION = "0.9.30-chronode.7";
 process.env["AGENTMEMORY_BUILD_ID"];
 process.env["AGENTMEMORY_VIEWER_BUILD_ID"];
 //#endregion
